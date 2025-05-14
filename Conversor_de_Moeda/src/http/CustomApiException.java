5696e665eb79501891a2f4e3d851ab4e49792413
Luiz Carlos T. Del Nero <161067539+DelNeroDados@users.noreply.github.com>
2025-05-14 19:58:58 -0300

    Add files via upload

---
+++ Conversor_de_Moeda/src/http/CustomApiException.java
@@ -0,0 +1,8 @@
+package http;
+
+// arquivo: src/http/CustomApiException.java
+public class CustomApiException extends Exception {
+    public CustomApiException(String msg) {
+        super(msg);
+    }
+}
---
+++ Conversor_de_Moeda/src/service/ConverterMoedaLivreApi.java
@@ -0,0 +1,18 @@
+/*package service;
+
+public record ConverterMoedaLivreApi(
+        String result,
+        String base_code,
+        String target_code,
+        double conversion_result
+) {}
+*/
+
+package service;
+
+public record ConverterMoedaLivreApi(
+        String result,
+        String base_code,
+        String target_code,
+        double conversion_result
+) {}
